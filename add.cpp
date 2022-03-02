169d2507b01fa637668ecfea6f9aed5ca86c4a57
harshbhoyar12 <100780942+harshbhoyar12@users.noreply.github.com>
2022-03-02 19:38:16 +0530

    Add files via upload

---
+++ add.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main ()
+{
+int num1,num2,ans;         //declaration
+num1=20;
+num2=30;                   //initialization
+ans=num1+num2;             //operation/logic
+printf("%d",ans);          //output
+  return 0;
+}
